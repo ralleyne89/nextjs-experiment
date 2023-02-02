c88910d813edb171bac65cdda1953c38287d4876
Reggie Alleyne <reggiealleyne89@gmail.com>
2023-02-02 02:07:42 -0800

    add images and details for movies

---
+++ nextjs-experiment/app/movie.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Movie({title, id, poster_path, release_date}) {
+    const imagePath = "https://image.tmdb.org/t/p/original"
+    return (
+        <div>
+            <h1>{title}</h1>
+            <h2>{release_date}</h2>
+            <Link href={`/${id}`}>
+                <Image src={imagePath + poster_path} alt={title} width={800} height={800} />
+            </Link>
+        </div>
+    )
+}
---
+++ nextjs-experiment/app/page.js
@@ -0,0 +1,26 @@
+import Movie from "./movie";
+
+export default async function Home() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  );
+  const res = await data.json();
+  console.log(res);
+  return (
+    <main>
+      <div className="grid gap-16 grid-cols-fluid">
+      {res.results.map((movie) => (
+        <div>
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            poster_path={movie.poster_path}
+            release_date={movie.release_date}
+          />
+        </div>
+      ))}
+      </div>
+    </main>
+  );
+}
